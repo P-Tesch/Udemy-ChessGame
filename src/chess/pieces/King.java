a92b85ccd6697bbb39c38da76b4cfa6c001ded1d
P-Tesch <pedrohenriquetesch@gmail.com>
2022-05-01 22:52:47 -0300

    Added King move logic

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -10,9 +11,26 @@ public class King extends ChessPiece {
 		super(board, color);
 	}
 	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) this.getBoard().piece(position);
+		return this.getBoard().positionExists(position) && (piece == null || piece.getColor() != this.getColor());
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] possibleMoves = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+		Position kingPosition = this.getPosition();
+		
+		//Clockwise
+		possibleMoves[kingPosition.getRow()][kingPosition.getColumn() - 1] = canMove(new Position(kingPosition.getRow(), kingPosition.getColumn() - 1));
+		possibleMoves[kingPosition.getRow() + 1][kingPosition.getColumn() - 1] = canMove(new Position(kingPosition.getRow() + 1, kingPosition.getColumn() - 1));
+		possibleMoves[kingPosition.getRow() + 1][kingPosition.getColumn()] = canMove(new Position(kingPosition.getRow() + 1, kingPosition.getColumn()));
+		possibleMoves[kingPosition.getRow() + 1][kingPosition.getColumn() + 1] = canMove(new Position(kingPosition.getRow() + 1, kingPosition.getColumn() + 1));
+		possibleMoves[kingPosition.getRow()][kingPosition.getColumn() + 1] = canMove(new Position(kingPosition.getRow(), kingPosition.getColumn() + 1));
+		possibleMoves[kingPosition.getRow() - 1][kingPosition.getColumn() + 1] = canMove(new Position(kingPosition.getRow() - 1, kingPosition.getColumn() + 1));
+		possibleMoves[kingPosition.getRow() - 1][kingPosition.getColumn()] = canMove(new Position(kingPosition.getRow() - 1, kingPosition.getColumn()));
+		possibleMoves[kingPosition.getRow() - 1][kingPosition.getColumn() - 1] = canMove(new Position(kingPosition.getRow() - 1, kingPosition.getColumn() - 1));
+		
 		return possibleMoves;
 	}
 
